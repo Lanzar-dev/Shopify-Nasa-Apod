9d0646e456d2181305d1bbf38460f922e699fc82
Toheeb <toheebyekeen@gmail.com>
2022-01-08 17:39:46 +0100

    fetch data

--- src/components/Apod/Apod.js
+++ src/components/Apod/Apod.js
@@ -8,16 +8,22 @@ export default function Apod() {
   const { date } = useParams();
 
   const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(
           `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`
         );
         setData(response.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setLoading(false);
+        setError(true);
       }
     };
     fetchData();
@@ -25,9 +31,25 @@ export default function Apod() {
 
   console.log(data);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error!</div>;
+  }
+
   return (
-    <div>
-      <p>{date}</p>
-    </div>
+    <article>
+      <h3>{data.title}</h3>
+      <p>
+        <em>{data.copyright}</em>
+      </p>
+      <figure>
+        <img src={data.url} alt={data.title} />
+        <figcaption>{data.title}</figcaption>
+      </figure>
+      <p>{data.date}</p>
+      <p>{data.explanation}</p>
+    </article>
   );
 }
